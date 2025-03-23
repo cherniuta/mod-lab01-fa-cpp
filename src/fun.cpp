630884ca866e1e3a231ca0e4063be0ba6575b905
cherniuta <kugemi@mail.ru>
2025-03-24 00:51:29 +0300

    Methods implementation

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,101 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str) {
+    unsigned int validWordsCount = 0;
+    bool isInsideWord = false;
+    bool containsDigits = false;
+
+    for (; *str; ++str) {
+        if (*str == ' ') {
+            if (isInsideWord) {
+                validWordsCount += !containsDigits;
+                isInsideWord = false;
+            }
+        }
+        else {
+            if (!isInsideWord) {
+                isInsideWord = true;
+                containsDigits = false;
+            }
+            containsDigits |= (*str >= '0' && *str <= '9');
+        }
+    }
+
+    if (isInsideWord) {
+        validWordsCount += !containsDigits;
+    }
+
+    return validWordsCount;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    unsigned int properCaseCount = 0;
+    bool isInsideWord = false;
+    bool isValidWord = false;
+    bool isFirstLetter = true;
+
+    while (*str) {
+        const char c = *str;
+        if (c != ' ') {
+            if (!isInsideWord) {
+                isInsideWord = true;
+                isValidWord = (c >= 'A' && c <= 'Z');
+                isFirstLetter = false;
+            }
+            else {
+                if (isFirstLetter) {
+                    isFirstLetter = false;
+                }
+                else {
+                    isValidWord &= (c >= 'a' && c <= 'z');
+                }
+            }
+        }
+        else {
+            if (isInsideWord) {
+                properCaseCount += isValidWord;
+                isInsideWord = false;
+                isFirstLetter = true;
+            }
+        }
+        ++str;
+    }
+
+    if (isInsideWord) {
+        properCaseCount += isValidWord;
+    }
+
+    return properCaseCount;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    unsigned int totalWords = 0;
+    unsigned int totalLength = 0;
+    unsigned int currentLength = 0;
+    bool isInsideWord = false;
+
+    for (; *str; ++str) {
+        if (*str == ' ') {
+            if (isInsideWord) {
+                ++totalWords;
+                totalLength += currentLength;
+                isInsideWord = false;
+            }
+        }
+        else {
+            if (!isInsideWord) {
+                isInsideWord = true;
+                currentLength = 0;
+            }
+            ++currentLength;
+        }
+    }
+
+    if (isInsideWord) {
+        ++totalWords;
+        totalLength += currentLength;
+    }
+
+    return totalWords ? (totalLength + totalWords / 2) / totalWords : 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
-    return 0;
+    std::cout << faStr1("One two 3 4 five./,") << std::endl;
+    std::cout << faStr2("One Tw2 three Four f/") << std::endl;
+    std::cout << faStr3("One Twoooooooooo Three34w") << std::endl;
 }
